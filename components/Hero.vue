a0e4ee07eadaf440dda7ce827c15f6dddb01973e
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-03-13 20:47:40 +0700

    1

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,16 +1,16 @@
 <template>
   <section class="text-cWhite bg-cGelap">
     <div
-      class="container flex flex-col items-center px-5 py-24 mx-auto md:flex-row"
+      class="container flex flex-col items-center px-4 mx-auto py-28 sm:px-16 md:flex-row"
     >
       <div
-        class="flex flex-col items-center mb-4 text-center lg:flex-grow md:w-1/2 md:items-start md:text-left "
+        class="flex flex-col items-center mb-4 text-center sm:mr-4 lg:flex-grow md:w-1/2 md:items-start md:text-left "
       >
         <h1
-          class="mb-4 text-4xl font-medium uppercase text-cTerang title-font sm:text-5xl"
+          class="mb-4 text-4xl font-medium uppercase text-cWhite title-font sm:text-5xl"
         >
           Welcome to<br class="hidden lg:inline-block" />
-          <span class="text-cUngu"> Indonesian Coffe Website </span>
+          <span class="text-cTerang"> Indonesian Coffe Website </span>
         </h1>
         <p class="mb-8 leading-relaxed">
           Indonesia is a country rich in natural wealth, one of the abundant
