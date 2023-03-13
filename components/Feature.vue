b52d7d10a145f45be81c6ec3f1eaf7678f8f1c0d
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-03-13 21:13:13 +0700

    1

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,24 +1,30 @@
 <template>
   <section class="bg-cPrimary">
-    <div class="container flex flex-col-reverse mx-auto lg:flex-row">
-      <div class="lg:w-1/2 xl:w-3/5 dark:bg-gray-800">
+    <div
+      class="flex flex-col-reverse items-center justify-center px-4 py-16 mx-auto sm:px-16 lg:flex-row"
+    >
+      <div class="w-full lg:w-1/2">
         <div class="flex items-center justify-center p-4 md:p-8 lg:p-12">
-			<TwicImg
-          class="object-cover object-center w-full transition duration-300 ease-in-out rounded hover:scale-95 h-[250]"
-          alt="hero"
-          title="hero"
-          src="/img/bijikopi.jpg"
-          format="webp"
-          fokus="left"
-          ratio="5/4"
-          resize="cover"
-          mode="cover"
-        />
+          <TwicImg
+            class="object-cover object-center w-full transition duration-300 ease-in-out rounded hover:scale-95"
+            alt="hero"
+            title="hero"
+            src="/img/bijikopi.jpg"
+            format="webp"
+            height="450"
+            width="450"
+			ratio="5/4"
+			fokus="left"
+			placeholder="preview"
+			resize="cover"
+			mode="cover"
+          />
         </div>
       </div>
       <div
-        class="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 text-cWhite"
+        class="flex flex-col w-full mb-4 space-y-6 rounded-sm sm:w-1/2 text-cWhite"
       >
+        <!-- 1 -->
         <div class="flex space-x-2 sm:space-x-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -35,15 +41,17 @@
             ></path>
           </svg>
           <div class="space-y-2">
-            <p class="text-lg font-medium leading-snug">
-              Lorem ipsum dolor sit amet
+            <p class="text-xl font-bold tracking-wide text-cTerang">
+              Indonesian coffee has a unique variety of flavors
             </p>
-            <p class="leading-snug">
-              Praesentium ea et neque distinctio quas eius repudiandae quaerat
-              obcaecati voluptatem similique!
+            <p class="text-base leading-snug">
+              For example, Aceh coffee that appears with a deep chocolate flavor
+              and there is also coffee that tastes like salak fruit to a spicy
+              taste.
             </p>
           </div>
         </div>
+        <!-- 2 -->
         <div class="flex space-x-2 sm:space-x-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -60,15 +68,17 @@
             ></path>
           </svg>
           <div class="space-y-2">
-            <p class="text-lg font-medium leading-snug">
-              Lorem ipsum dolor sit amet
+            <p class="text-xl font-bold tracking-wide text-cTerang">
+              The best type of coffee in the world is in Indonesia, namely Kopi
+              Luwak or civet coffee
             </p>
-            <p class="leading-snug">
-              Praesentium ea et neque distinctio quas eius repudiandae quaerat
-              obcaecati voluptatem similique!
+            <p class="text-base leading-snug">
+              Kopi luwak is the coffee with the best quality, making it a
+              typical Indonesian coffee
             </p>
           </div>
         </div>
+        <!-- 3 -->
         <div class="flex space-x-2 sm:space-x-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -85,12 +95,13 @@
             ></path>
           </svg>
           <div class="space-y-2">
-            <p class="text-lg font-medium leading-snug">
-              Lorem ipsum dolor sit amet
+            <p class="text-xl font-bold tracking-wide text-cTerang">
+              Many flavor options because it has a variety of types of coffee in
+              each region
             </p>
-            <p class="leading-snug">
-              Praesentium ea et neque distinctio quas eius repudiandae quaerat
-              obcaecati voluptatem similique!
+            <p class="text-base leading-snug">
+              Coffee lovers will feel happy to come to Indonesia, because every
+              region in Indonesia has its own specialty coffee
             </p>
           </div>
         </div>
